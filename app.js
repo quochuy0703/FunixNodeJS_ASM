7ac9341afdf1a73ccd63c1125bab011ca6be7061
Huy Mai <quochuy0703@gmail.com>
2022-07-12 23:04:38 +0700

    commit_asm1_2

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello world!</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server run!");
+});
